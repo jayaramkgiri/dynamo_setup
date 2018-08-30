8b2d0510710bf0712f654d9bf6a02a910ad26912
jayaramkgiri <>
2018-08-30 23:29:53 +0530

    remove keys

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
   Aws.config.update({
-    region: 'ap-south-1',
-    credentials: Aws::Credentials.new('AKIAILEEAPGE2CUZWK4A', '1IpTbE2hWV/c2zhLWifchHNFL14TWcbHyI9BP9BT'),
+    region: 'us-west-2',
+    credentials: Aws::Credentials.new('REPLACE_WITH_ACCESS_KEY_ID', 'REPLACE_WITH_SECRET_ACCESS_KEY'),
   })
